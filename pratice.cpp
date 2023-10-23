8b2b5e32fd1bc5b2f96cc51cc49a51be13a6d906
YashaAmir <148742424+YashaAmir@users.noreply.github.com>
2023-10-22 22:51:29 -0700

    Add files via upload

---
+++ pratice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string firstname;
+	string lastname;
+	cout<<"please enter first name";
+	cin>>firstname;
+	cout<<"please enter last name";
+	cin>>lastname;
+	if (firstname==lastname)
+	{
+		cout<<"names are equal";
+	}
+	else
+	cout<<"name are not equal";
+}
